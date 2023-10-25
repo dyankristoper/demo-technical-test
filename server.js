c6eb9eee1b43ab9d101892e47698b69dc7a6fb2b
JC <christophera@balboadigital.com>
2023-10-26 06:06:58 +0800

    Fix: Changed app to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 server.use(express.urlencoded({ extended: true })); 
 server.use(express.json({extended: true}));
 
-app.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 
 // server.get('/', async (request, response) => {
 //     response.sendFile(path.join(__dirname, 'public', 'index.html'));
