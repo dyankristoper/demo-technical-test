8937e7c46462ca6a692e35ea0b6c05780ba1813b
JC <christophera@balboadigital.com>
2023-10-26 03:33:39 +0800

    Fix: Update port, case

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const si = require('systeminformation');
 
 const getSystemInfo = async () => {
